2213a46d1fff078d605ce637f8cdef432faa898b
markovichecha <an.mark96@gmail.com>
2021-04-15 03:21:10 +0300

    2.33

---
+++ 2/33.rkt
@@ -0,0 +1,22 @@
+#lang racket
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) '() sequence))
+
+(map (lambda (x) (* x 2)) (list 1 2 3))
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(append (list 1 2) (list 3 4))
+
+(define (length sequence)
+  (accumulate (lambda (x y) (+ y 1)) 0 sequence))
+
+(length (list 1 2 3 4 5))
